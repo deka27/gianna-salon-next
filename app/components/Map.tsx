c353dcdf077ce751f4655bbabccd54f16006f149
Kaustav Deka <116963260+deka27@users.noreply.github.com>
2023-09-05 23:56:30 +0530

    changes to map

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -1,4 +1,5 @@
 import {MdOutlineShareLocation} from 'react-icons/md';
+import Iframe from 'react-iframe'
 
 export default function Map() {
   return (
@@ -10,13 +11,13 @@ export default function Map() {
         <div className="text-center w-[60%] my-2">3, 19th main, 27th Main Rd, opposite Nagarjuna appartment, 2nd Sector, HSR Layout, Bengaluru, Karnataka 560102</div>
       </div>
       <div className="w-[80%] h-[500px] mx-auto rounded-3xl my-10">
-      <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3889.0491894105767!2d77.64317697685317!3d12.904558616341111!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae152e97e15d2f%3A0x246fc925e4c509c1!2sGIANNA%20BEAUTY%20SALON!5e0!3m2!1sen!2sin!4v1693934804120!5m2!1sen!2sin"
+      <Iframe
+        url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3889.0491894105767!2d77.64317697685317!3d12.904558616341111!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae152e97e15d2f%3A0x246fc925e4c509c1!2sGIANNA%20BEAUTY%20SALON!5e0!3m2!1sen!2sin!4v1693934804120!5m2!1sen!2sin"
         width="100%"
         height="100%"
         loading="lazy"
         referrerpolicy="no-referrer-when-downgrade"
-      ></iframe>
+      ></Iframe>
       </div>
     </div>
   );
